c8b2bdb238ed67b3da06554d768ca30990114489
TheNotary <no@mail.plz>
2013-05-05 12:29:47 -0500

    git rid of a lot of useless unit::Test files

--- test/functional/cute_lil_functions_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class CuteLilFunctionsControllerTest < ActionController::TestCase
-  test "should get get_matrix_html_from_dimensions" do
-    get :get_matrix_html_from_dimensions
-    assert_response :success
-  end
-
-end
--- test/unit/helpers/cute_lil_functions_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class CuteLilFunctionsHelperTest < ActionView::TestCase
-end
